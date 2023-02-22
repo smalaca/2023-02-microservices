1566469662459e748d3f4bcda30cbfd704b3a0e3
smalaca <ab13.krakow@gmail.com>
2023-02-22 17:07:57 +0100

    add OrderNumber generation

--- src/main/java/com/smalaca/purchase/command/domain/order/OrderNumber.java
+++ src/main/java/com/smalaca/purchase/command/domain/order/OrderNumber.java
@@ -1,7 +1,15 @@
 package com.smalaca.purchase.command.domain.order;
 
+import java.util.UUID;
+
 class OrderNumber {
+    private final String value;
+
+    private OrderNumber(String value) {
+        this.value = value;
+    }
+
     static OrderNumber generate() {
-        return null;
+        return new OrderNumber(UUID.randomUUID().toString());
     }
 }
