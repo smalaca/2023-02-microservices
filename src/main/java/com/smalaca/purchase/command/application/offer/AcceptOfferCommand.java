4f3111a044b204180cc236838b4249d7e9f324be
smalaca <ab13.krakow@gmail.com>
2023-02-23 10:43:31 +0100

    add NotNull validation over AcceptOfferCommand

--- src/main/java/com/smalaca/purchase/command/application/offer/AcceptOfferCommand.java
+++ src/main/java/com/smalaca/purchase/command/application/offer/AcceptOfferCommand.java
@@ -2,10 +2,12 @@ package com.smalaca.purchase.command.application.offer;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Getter
 public class AcceptOfferCommand {
+    @NotNull
     private UUID offerId;
     private String discountCode;
     private String deliveryMethod;
